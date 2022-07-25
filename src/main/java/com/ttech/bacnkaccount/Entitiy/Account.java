2e68c157a92ef61082f175212532b767ca20d06e
aravindsn <aravind@ttechlab.com>
2022-07-25 10:52:33 +0530

    tried changing Owner set (still broken)

--- src/main/java/com/ttech/bacnkaccount/Entitiy/Account.java
+++ src/main/java/com/ttech/bacnkaccount/Entitiy/Account.java
@@ -9,6 +9,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.ttech.bacnkaccount.Controllers.CustomerService;
+
 @Entity
 @Table(name = "Accounts")
 public class Account {

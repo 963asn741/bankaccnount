b531332c879fb604891479edc699a5c46fd3c1f1
aravindsn <aravind@ttechlab.com>
2022-07-26 12:55:53 +0530

    tried something didnt work

--- src/main/java/com/ttech/bacnkaccount/Controllers/CustomerController.java
+++ src/main/java/com/ttech/bacnkaccount/Controllers/CustomerController.java
@@ -2,17 +2,21 @@ package com.ttech.bacnkaccount.Controllers;
 
 import com.ttech.bacnkaccount.Entitiy.Customer;
 
+import java.lang.ProcessBuilder.Redirect;
 import java.util.*;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -37,7 +41,16 @@ public class CustomerController {
     }
 
     @PostMapping("/customers/add-customer")
-    public void addCustomerToTable(@RequestBody Customer newCustomer){
+    public String addCustomerToTable(@ModelAttribute Customer newCustomer){
         customerService.addCustomer(newCustomer);
+        return "redirect:/customer/list";
     }
+
+    @GetMapping("/customer/list")
+    public String getList(Model model){
+        model.addAttribute("listcustomer",customerService.getAllCustomers());
+        return "list-of-customers";
+    }
+   
+
 }

35e8f81285c3e160a6dd7c9edd62daeb98ef296a
aravindsn <aravind@ttechlab.com>
2022-08-05 17:44:15 +0530

    fixed responsiveness of some pages

--- src/main/java/com/ttech/bacnkaccount/BacnkaccountApplication.java
+++ src/main/java/com/ttech/bacnkaccount/BacnkaccountApplication.java
@@ -3,44 +3,8 @@ package com.ttech.bacnkaccount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-ROUTES TO DO STUFF:
-/customers : returns all customers in DB [GET]
-
-/accounts : returns all accounts in DB [GET]
-
-/transactions : returns all transactions in DB [GET]
-
-/customers/{customerId} : returns customer with id=customerId [GET]
-
-/customers/add-customer : to add new customer [POST]
-	{
-		"id" : 1,
-		"name" : "Alex"
-	}
-
-/customers/{customerId}/accounts : returns all account of customer with id=customerId [GET]
-
-/customers/accounts/{accountId} : returns account with id=accountId [GET]
-
-/customers/{customerId}/add-account : add new account to DB [POST]
-	{
-		"id" : 1000,
-		"balance : 0"	
-	}
-
-
-/customers/{customerId}/{accountId}/transactions : return all transactions of account with id=accountId [GET]
-
-/customers/{customerId}/{accountId}/transactions/{transactionId} : reutrns transaction with id=transactionId [GET]
-
-/customers/{customerId}/{accountId}/new-transaction : adds a transaction to DB and updates account balance value [POST]
-	{
-		"id" : 123456789
-		"amount" : 30000
-	}
-*/
-
+//running on port 3939
+//maybe address = 193.168.1.187:3939
 
 @SpringBootApplication
 public class BacnkaccountApplication {

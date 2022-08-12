63ebbe22dbaa053d520b5c538b0d373bf22e300a
aravindsn <aravind@ttechlab.com>
2022-08-12 12:04:26 +0530

    fixed button icons and loginpage bg

--- src/main/java/com/ttech/bacnkaccount/Controllers/AccountController.java
+++ src/main/java/com/ttech/bacnkaccount/Controllers/AccountController.java
@@ -1,8 +1,5 @@
 package com.ttech.bacnkaccount.Controllers;
 
-import java.security.Principal;
-import java.util.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;

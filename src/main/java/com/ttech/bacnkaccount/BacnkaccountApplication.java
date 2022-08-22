cb402da576b8728f09589915d0cd3563000cf35e
aravindsn <aravind@ttechlab.com>
2022-08-22 17:12:32 +0530

    added check admin in DB after boot up

--- src/main/java/com/ttech/bacnkaccount/BacnkaccountApplication.java
+++ src/main/java/com/ttech/bacnkaccount/BacnkaccountApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BacnkaccountApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BacnkaccountApplication.class, args);
 	}
--- src/main/java/com/ttech/bacnkaccount/Controllers/MyUserDetailsService.java
+++ src/main/java/com/ttech/bacnkaccount/Controllers/MyUserDetailsService.java
@@ -11,17 +11,38 @@ import org.springframework.stereotype.Service;
 import com.ttech.bacnkaccount.Entitiy.User;
 import com.ttech.bacnkaccount.JPA.UserRepo;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserRepo userRepo;
 
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "admin";
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepo.findByUsername(username);
-
         user.orElseThrow(() -> new UsernameNotFoundException("Not found: "+ username));
         return user.map(MyUserDetails::new).get();
+    }
+
+    @PostConstruct
+    public void checkAdmin(){
+        System.out.println("Running check for admin user");
+        if (userRepo.findByUsername("admin").isEmpty()){
+            User admin = new User();
+            admin.setUsername(USERNAME);
+            admin.setPassword(PASSWORD);
+            admin.setActive(true);
+            admin.setId(1);
+            admin.setRoles("ROLE_ADMIN");
+            admin.setName("Admin");
+            userRepo.save(admin);
+            System.out.println("Admin user added");
+        }
+        else System.out.println("Admin user already exists");
     }
 }
